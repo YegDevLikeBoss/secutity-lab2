e4bfebab4056d5e0f41ea8c0bfd4eabacd2f4bf4
Artyom Yeghoyan <artyomyeghoyan@gmail.com>
2020-12-16 20:56:13 +0300

    lab4 refactoring

--- lab4/test.py
+++ lab4/test.py
@@ -7,48 +7,48 @@ if __name__ == '__main__':
     I = 'alice'
     P = 'password123'
     print(f'I = {I}, P = {P}')
-    
-    print('Client: computes verifier')
+
+    print('Клиент: рассчитывает верификатор')
     salt, verifier = client.compute_verifier(I, P)
-    print(f'Salt is: {salt}')
-    print(f'Verifier is: {verifier}')
+    print(f'Соль: {salt}')
+    print(f'Верификатор: {verifier}')
 
-    print('Client->I, s, v->Server')
-    print('Client: computes private and public values')
+    print('Клиент передаёт I, s, v на сервер')
+    print('Клиент: рассчитывает частные и публичные значения')
     A = client.compute_client_values()
-    print(f'CLient A: {A}')
+    print(f'Клиент A: {A}')
 
-    print('Client->A->Server')
-    print('Server: computes private and public values')
+    print('Клиент передаёт A на сервер')
+    print('Сервер: рассчитывает частные и публичные значения')
     B = serv.compute_server_values(I, verifier)
-    print(f'Server B: {B}')
+    print(f'Сервер B: {B}')
 
-    print('Server->B->CLient')
-    print('Client: computes premaster secret and session key')
+    print('Сервер передаёт B клиенту')
+    print('Клиент: рассчитывает скремблер и сессионный ключ')
     client.compute_premaster_secret(salt, B)
     M = client.compute_session_key(salt, B)
 
-    print('Server: computes premaster secret and session key')
+    print('Сервер: рассчитывает скремблер и сессионный ключ')
     serv.compute_premaster_secret(I, salt, verifier, A)
     serv.compute_session_key(I, salt, A)
 
-    print(f'Client evidence message: {M}')
-    print(f'Server evidence message: {serv.M}')
+    print(f'Сообщение существования клиента: {M}')
+    print(f'Сообщение существования сервера: {serv.M}')
 
-    print('Client->M->Server')
-    print('Server: verifies session')
+    print('Клиент передаёт M на сервер')
+    print('Сервер: верифицирует сессию')
     hashed_M = serv.verify_session(M)
-    print(f'Server hashed_M: {hashed_M}')
+    print(f'Сервер: захешированный M: {hashed_M}')
 
-    print('Server->hashed_M->Client')
-    print('Client: verifies session')
+    print('Сервер передаёт захешированный M Клиенту')
+    print('Клиент: Верифицирует сессию')
     hashed_M = client.verify_session(hashed_M)
-    print(f'Client hashed_M: {hashed_M}')
+    print(f'Клиент: захешированный M: {hashed_M}')
 
-    print(f'Client session key: {client.session_key}')
-    print(f'Server session key: {serv.session_key}')
+    print(f'Сессионный ключ клиента: {client.session_key}')
+    print(f'Сессионный ключ сервера: {serv.session_key}')
 
     assert client.authenticated
     assert serv.authenticated
 
-    print('Success')
+    print('Удача')
